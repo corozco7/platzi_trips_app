5f0fd616cfa8c516594ad2f1c2898023112cc509
Cesar Orozco <c.orozco1293@gmail.com>
2021-11-28 11:41:50 -0500

    Hola mundo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -24,7 +22,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hola mundo feliz"),
+        ),
+        body: const Center(
+          child: Text("Hola mundo Platzi"),
+        ),
+      )//const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
